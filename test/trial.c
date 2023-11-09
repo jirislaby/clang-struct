51daa49c3c8740a7fdc825211e57792069a66eda
Jiri Slaby <jirislaby@gmail.com>
2023-11-09 12:13:34 +0100

    trial: add some anon members

--- test/trial.c
+++ test/trial.c
@@ -10,14 +10,25 @@ struct X {
 	int used_ptr;
 };
 
+struct my_anon {
+	struct {
+		int anon_member;
+	};
+};
+
+struct {
+	int anon_member;
+} my_anon2;
+
 struct PACKED {
 	int used_by_hw;
 } __attribute__((packed,aligned(128)));
 
-void fun(struct X *x)
+void fun(struct X *x, struct my_anon *y)
 {
 	x->wr = 1;
-	x->used = 1;
+	x->used = my_anon2.anon_member;
+	y->anon_member = 1;
 }
 
 int fun2(struct X *y, struct X z)
