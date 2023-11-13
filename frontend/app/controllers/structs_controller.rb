e849bf8333a9ad20ef8ddf98288fee2d657050cf
Jiri Slaby <jirislaby@gmail.com>
2023-11-13 13:14:46 +0100

    handle nested structures

--- frontend/app/controllers/structs_controller.rb
+++ frontend/app/controllers/structs_controller.rb
@@ -17,7 +17,14 @@ class StructsController < ApplicationController
   end
 
   def show
-    @struct = MyStruct.joins(:source, :member).find(params[:id])
+    @struct = MyStruct.joins(:source).select('struct.*', 'source.src AS src_file').find(params[:id])
+    @members = Member.select('member.*',
+                             "(SELECT id FROM struct AS nested " <<
+                             "WHERE nested.src = #{@struct.src} AND " <<
+                             "member.begLine == nested.begLine AND " <<
+                             "member.begCol == nested.begCol LIMIT 1) " <<
+                             "AS nested_id").
+      where(struct: @struct)
 
     respond_to do |format|
       format.html
