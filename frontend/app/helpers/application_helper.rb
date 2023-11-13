ea4b9aaa94cd05eca58562a3b6cad94199b22ece
Jiri Slaby <jirislaby@gmail.com>
2023-11-13 10:26:53 +0100

    add and use src_link_to helper

--- frontend/app/helpers/application_helper.rb
+++ frontend/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def src_link_to(text, file, line = 0)
+    @rev = "v6.6"
+    @link = "https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/#{file}?id=#{@rev}"
+    if line > 0
+      @link << "#n#{line}"
+    end
+    link_to(text, @link)
+  end
 end
