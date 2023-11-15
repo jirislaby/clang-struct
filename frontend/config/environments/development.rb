813ef25cdfdfff023cb45f6b7f400767231733f5
Jiri Slaby <jirislaby@gmail.com>
2023-11-15 09:03:47 +0100

    development: compile assets on the fly

--- frontend/config/environments/development.rb
+++ frontend/config/environments/development.rb
@@ -62,6 +62,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = true
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
