jirislaby/clang-struct

commit e7e63c85c74a4feb3d1d39415ebcdb4be3c67d62

Message: use string_view in db_filler
To avoid copies of the string all over.

src/db_filler.cpp
@@ -20,6 +20,8 @@
 #include "sqlite.h"
 #include "Message.h"
 
+using Msg = Message<std::string_view>;
+
 volatile std::sig_atomic_t stop;
 
 class Server {
@@ -30,7 +32,7 @@ public:
 	int open();
 	void close();
 
-	std::string read();
+	std::string_view read();
 private:
 #if 0
 	int sock = -1;
@@ -50,7 +52,7 @@ Server::~Server()
 	close();
 }
 
-std::string Server::read()
+std::string_view Server::read()
 {
 	auto rd = mq_receive(mq, buf.get(), buf_len, NULL);
 	if (rd < 0) {
@@ -59,7 +61,7 @@ std::string Server::read()
 		return "";
 	}
 
-	return std::string(buf.get(), rd);
+	return std::string_view(buf.get(), rd);
 }
 
 void Server::close()
@@ -358,9 +360,9 @@ public:
 	int begin();
 	int end();
 
-	int handleMessage(const Message &msg);
+	int handleMessage(const Msg &msg);
 private:
-	int bindAndStep(SQLStmtHolder &ins, const Message &msg);
+	int bindAndStep(SQLStmtHolder &ins, const Msg &msg);
 
 	SQLHolder sqlHolder;
 	SQLStmtHolder insSrc;
@@ -592,7 +594,7 @@ int SQLConn::end()
 	return 0;
 }
 
-int SQLConn::bindAndStep(SQLStmtHolder &ins, const Message &msg)
+int SQLConn::bindAndStep(SQLStmtHolder &ins, const Msg &msg)
 {
 	SQLStmtResetter insSrcResetter(sqlHolder, ins);
 	int ret;
@@ -600,18 +602,21 @@ int SQLConn::bindAndStep(SQLStmtHolder &ins, const Message &msg)
 	for (auto e: msg) {
 		const auto [type, key, val] = e;
 
-		auto bindIdx = sqlite3_bind_parameter_index(ins, (":" + key).c_str());
+		std::string bind(":");
+		bind.append(key);
+		auto bindIdx = sqlite3_bind_parameter_index(ins, bind.c_str());
 		if (!bindIdx) {
 			std::cerr << "no index found for key=" << key << "\n";
 			std::cerr << "\t" << msg << "\n";
 			return -1;
 		}
 
-		if (type == Message::TYPE::TEXT) {
-			ret = sqlite3_bind_text(ins, bindIdx, val.c_str(), -1, SQLITE_TRANSIENT);
-		} else if (type == Message::TYPE::INT) {
+		if (type == Msg::TYPE::TEXT) {
+			ret = sqlite3_bind_text(ins, bindIdx, val.data(), -1, SQLITE_TRANSIENT);
+		} else if (type == Msg::TYPE::INT) {
 			try {
-				ret = sqlite3_bind_int(ins, bindIdx, std::stoi(val));
+				auto i = std::stoi(std::string(val));
+				ret = sqlite3_bind_int(ins, bindIdx, i);
 			} catch (std::invalid_argument const& ex) {
 				std::cerr << "bad int val=\"" << val << "\"\n";
 				return -1;
@@ -642,17 +647,17 @@ int SQLConn::bindAndStep(SQLStmtHolder &ins, const Message &msg)
 	return 0;
 }
 
-int SQLConn::handleMessage(const Message &msg)
+int SQLConn::handleMessage(const Msg &msg)
 {
 	auto kind = msg.getKind();
 
-	if (kind == Message::KIND::SOURCE)
+	if (kind == Msg::KIND::SOURCE)
 		return bindAndStep(insSrc, msg);
-	if (kind == Message::KIND::STRUCT)
+	if (kind == Msg::KIND::STRUCT)
 		return bindAndStep(insStr, msg);
-	if (kind == Message::KIND::MEMBER)
+	if (kind == Msg::KIND::MEMBER)
 		return bindAndStep(insMem, msg);
-	if (kind == Message::KIND::USE)
+	if (kind == Msg::KIND::USE)
 		return bindAndStep(insUse, msg);
 
 	std::cerr << "bad message kind: " << kind << "\n";
@@ -692,7 +697,7 @@ int main()
 		if (stop || msgStr.empty())
 			break;
 
-		auto msg = Message::deserialize(msgStr);
+		auto msg = Msg::deserialize(msgStr);
 
 		//std::cerr << "===" << msg << "\n";
 

src/Message.h
@@ -1,13 +1,16 @@
 #ifndef MESSAGE_H
 #define MESSAGE_H
 
+#include <cassert>
 #include <cstdint>
+#include <cstring>
 #include <ostream>
 #include <string>
 #include <sstream>
 #include <tuple>
 #include <vector>
 
+template<typename T>
 class Message {
 public:
 	enum TYPE {
@@ -20,21 +23,21 @@ public:
 		MEMBER = 'M',
 		USE = 'U',
 	};
-	using entry = std::tuple<TYPE, std::string, std::string>;
+	using entry = std::tuple<TYPE, const T, const T>;
 	using storage = std::vector<entry>;
 
 	Message(const KIND &kind) : kind(kind) {}
 
-	void add(const TYPE &type, const std::string &key, const std::string &val) {
+	void add(const TYPE &type, const T &key, const T &val) {
 		entries.push_back(std::make_tuple(type, key, val));
 	}
 
-	void add(const std::string &key, const std::string &val) {
+	void add(const T &key, const T &val) {
 		add(TEXT, key, val);
 	}
 
-	template<typename T>
-	void add(const std::string &key, T val) {
+	template<typename U>
+	void add(const T &key, U val) {
 		add(INT, key, std::to_string(val));
 	}
 
@@ -46,42 +49,42 @@ public:
 	KIND getKind() const { return kind; }
 	size_t size() const { return entries.size(); }
 	entry operator[](int idx) const { return entries[idx]; }
-	storage::const_iterator begin() const { return entries.begin(); }
-	storage::const_iterator end() const { return entries.end(); }
+	typename storage::const_iterator begin() const { return entries.begin(); }
+	typename storage::const_iterator end() const { return entries.end(); }
 
 	std::string serialize() const;
-	static Message deserialize(const std::string &str);
+	static Message deserialize(const std::string_view &str);
 private:
 	Message() {}
 
 	void setKind(const KIND &kind) { this->kind = kind; }
 
 	static void serializeString(std::stringstream &ss, const std::string &str);
-	static std::string deserializeString(std::istringstream &ss);
+	static std::string_view deserializeString(std::string_view &str);
 
 	KIND kind;
 	storage entries;
 };
 
-inline void Message::serializeString(std::stringstream &ss, const std::string &str)
+template<typename T> inline void Message<T>::serializeString(std::stringstream &ss, const std::string &str)
 {
 	uint16_t sz = str.length();
 	ss.write((char *)&sz, sizeof(sz));
 	ss << str;
 }
 
-inline std::string Message::deserializeString(std::istringstream &ss)
+template<typename T> inline std::string_view Message<T>::deserializeString(std::string_view &str)
 {
-	uint16_t sz;
-	ss.read((char *)&sz, sizeof(sz));
+	using slen = uint16_t;
+	assert(str.length() > sizeof(slen));
 
-	char buf[sz];
-	ss.read(buf, sz);
-
-	return std::string(buf, sz);
+	auto len = *(slen *)str.data();
+	auto ret = str.substr(sizeof(slen), len);
+	str = str.substr(sizeof(slen) + len);
+	return ret;
 }
 
-inline std::string Message::serialize() const
+template<typename T> inline std::string Message<T>::serialize() const
 {
 	std::stringstream ss;
 
@@ -98,19 +101,19 @@ inline std::string Message::serialize() const
 	return ss.str();
 }
 
-inline Message Message::deserialize(const std::string &str)
+template<typename T> inline Message<T> Message<T>::deserialize(const std::string_view &str)
 {
-	std::istringstream iss(str);
-	std::string item;
-	Message msg((KIND)iss.get());
+	Message msg((KIND)str[0]);
+	auto cur = str.substr(1);
 
-	while (true) {
-		auto type = iss.get();
+	while (cur.length()) {
+		auto type = cur[0];
 		if (type == EOF)
 			break;
 
-		auto key = deserializeString(iss);
-		auto val = deserializeString(iss);
+		cur = cur.substr(1);
+		auto key = deserializeString(cur);
+		auto val = deserializeString(cur);
 
 		msg.add((TYPE)type, key, val);
 	}
@@ -118,7 +121,7 @@ inline Message Message::deserialize(const std::string &str)
 	return msg;
 }
 
-inline std::ostream& operator<<(std::ostream &os, const Message &msg)
+template<typename T> inline std::ostream& operator<<(std::ostream &os, const Message<T> &msg)
 {
 	os.put(msg.getKind());
 

src/clang-struct.cpp
@@ -19,6 +19,7 @@
 using namespace clang;
 using namespace clang::ast_matchers;
 using namespace clang::ento;
+using Msg = Message<std::string>;
 
 class Connection {
 public:
@@ -26,7 +27,7 @@ public:
 	~Connection();
 
 	int open();
-	void write(const Message &msg);
+	void write(const Msg &msg);
 private:
 #if 0
 	int sock = -1;
@@ -51,7 +52,7 @@ public:
 
 	void run(const MatchFinder::MatchResult &res);
 private:
-	void bindLoc(Message &msg, const SourceRange &SR);
+	void bindLoc(Msg &msg, const SourceRange &SR);
 	std::string getSrc(const SourceLocation &SLOC);
 
 	void handleUse(const MemberExpr *ME, const RecordType *ST);
@@ -110,7 +111,7 @@ int Connection::open()
 	return 0;
 }
 
-void Connection::write(const Message &msg)
+void Connection::write(const Msg &msg)
 {
 #if 0
 	auto ret = ::write(sock, msg.c_str(), msg.length());
@@ -133,7 +134,7 @@ void Connection::write(const Message &msg)
 #endif
 }
 
-void MatchCallback::bindLoc(Message &msg, const SourceRange &SR)
+void MatchCallback::bindLoc(Msg &msg, const SourceRange &SR)
 {
 	msg.add("begLine", SM.getPresumedLineNumber(SR.getBegin()));
 	msg.add("begCol", SM.getPresumedColumnNumber(SR.getBegin()));
@@ -158,12 +159,12 @@ void MatchCallback::handleUse(const MemberExpr *ME, const RecordType *ST)
 	auto strLoc = ST->getDecl()->getBeginLoc();
 	auto strSrc = getSrc(strLoc);
 	auto useSrc = getSrc(ME->getBeginLoc());
-	Message msg(Message::KIND::SOURCE);
+	Msg msg(Msg::KIND::SOURCE);
 
 	msg.add("src", useSrc);
 	conn.write(msg);
 
-	msg.renew(Message::KIND::USE);
+	msg.renew(Msg::KIND::USE);
 	msg.add("member", getNDName(ME->getMemberDecl()));
 	msg.add("struct", getRDName(ST->getDecl()));
 	msg.add("strSrc", strSrc);
@@ -222,12 +223,12 @@ void MatchCallback::handleRD(const RecordDecl *RD)
 	auto RDSR = RD->getSourceRange();
 	auto RDName = getRDName(RD);
 	auto src = getSrc(RDSR.getBegin());
-	Message msg(Message::KIND::SOURCE);
+	Msg msg(Msg::KIND::SOURCE);
 
 	msg.add("src", src);
 	conn.write(msg);
 
-	msg.renew(Message::KIND::STRUCT);
+	msg.renew(Msg::KIND::STRUCT);
 	msg.add("name", RDName);
 
 	std::stringstream ss;
@@ -257,7 +258,7 @@ void MatchCallback::handleRD(const RecordDecl *RD)
 		/*llvm::errs() << __func__ << ": " << RD->getNameAsString() <<
 				"." << f->getNameAsString() << "\n";*/
 		auto SR = f->getSourceRange();
-		msg.renew(Message::KIND::MEMBER);
+		msg.renew(Msg::KIND::MEMBER);
 		msg.add("name", getNDName(f));
 		msg.add("struct", RDName);
 		msg.add("src", src);

commit 93fb1bc37a67ab5b2129bd0b6ca274f38dda1b68

db_filler: fix string_view rewrite
We did not limit the count when binding strings.

Fixes: e7e63c85c74a

src/db_filler.cpp
@@ -439,7 +439,7 @@ int SQLConn::bindAndStep(SQLStmtHolder &ins, const Msg &msg)
 		}
 
 		if (type == Msg::TYPE::TEXT) {
-			ret = sqlite3_bind_text(ins, bindIdx, val.data(), -1, SQLITE_TRANSIENT);
+			ret = sqlite3_bind_text(ins, bindIdx, val.data(), val.length(), SQLITE_TRANSIENT);
 		} else if (type == Msg::TYPE::INT) {
 			try {
 				auto i = std::stoi(std::string(val));