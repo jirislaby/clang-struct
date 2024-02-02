6c3687b129de3552b4dc2a62fc110ad1a9036376
Jiri Slaby <jirislaby@gmail.com>
2024-02-02 08:24:06 +0100

    frontend: switch to kernel 6.7

--- frontend/app/helpers/application_helper.rb
+++ frontend/app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def src_link_to(text, file, line = 0)
-    @rev = "v6.6"
+    @rev = "v6.7"
     @link = "https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/#{file}?id=#{@rev}"
     if line > 0
       @link << "#n#{line}"
