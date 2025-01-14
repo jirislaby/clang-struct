846b4edf28967d26426d8a4dc3e76199dcdcb00a
Jiri Slaby <jirislaby@gmail.com>
2025-01-14 10:50:06 +0100

    frontend: uses: add pager too

--- frontend/app/controllers/uses_controller.rb
+++ frontend/app/controllers/uses_controller.rb
@@ -10,7 +10,30 @@ class UsesController < ApplicationController
   def show
     @member = Member.joins(:struct).find(params[:id])
     @uses = Use.where(member: @member).left_joins(:run)
-    @uses_count = @uses.count
+
+    listing_limit_cropped = listing_limit * 3 + 1
+
+    @page = @offset = 0
+    unless params[:page].blank?
+      @page = params[:page].to_i
+      @offset = @page * listing_limit
+      @uses = @uses.offset(@offset)
+    end
+    @uses = @uses.limit(listing_limit_cropped)
+    @uses_all_count = @uses.count # ALL COUNT
+    @uses = @uses.limit(listing_limit)
+    @uses_count = @uses.count # COUNT
+
+    @uses_all_count += @offset
+    if @uses_all_count > @offset + listing_limit
+      if @uses_all_count >= @offset + listing_limit_cropped
+        @uses_all_count = "many"
+      end
+      @next_page = @page + 1
+    else
+      @next_page = 0
+    end
+
     @uses = @uses.joins(:source).
       select('use.*', 'run.version', 'source.src AS src_file').order('src_file, begLine')
 
